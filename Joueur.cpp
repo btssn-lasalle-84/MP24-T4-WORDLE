03c24524f2260d5ac8d4b95957cd0fcc1e61e663
OthmanLatyaoui <othmanlatyaoui.pro@gmail.com>
2023-11-30 10:45:43 +0100

    MP24T4-5 Déclaration et définition de la classe Joueur

--- Joueur.cpp
+++ Joueur.cpp
@@ -1,1 +1,62 @@
 #include "Joueur.h"
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+#define NB_ENTREES_MAX 6
+
+Joueur::Joueur() : scoreActuel(0), nbTentatives(0)
+{
+}
+
+int Joueur::score() const
+{
+    return scoreActuel;
+}
+
+int Joueur::incrementerScore()
+{
+    scoreActuel += 2;
+    return scoreActuel;
+}
+
+int Joueur::tentativesRestantes() const
+{
+    if(nbTentatives < NB_ENTREES_MAX)
+    {
+        return NB_ENTREES_MAX - nbTentatives;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+char Joueur::entreeJoueur()
+{
+    char entree;
+    cout << "Veuillez entrer une lettre : ";
+    cin >> entree;
+
+    return entree;
+}
+
+void Joueur::afficherScore() const
+{
+    cout << "Score : " << score() << endl;
+}
+
+void Joueur::reinitialiserJeu()
+{
+    scoreActuel  = 0;
+    nbTentatives = 0;
+    motsProposes.clear();
+}
+
+void Joueur::proposerMot(std::string motPropose)
+{
+    motsProposes.push_back(motPropose);
+}
+
--- Joueur.h
+++ Joueur.h
@@ -1,6 +1,26 @@
-
+#ifndef JOUEUR_H
 #define JOUEUR_H
+
+#include <vector>
+#include <string>
+#include <iostream>
 
 class Joueur
 {
+  private:
+    std::vector<std::string> motsProposes;
+    int                      scoreActuel;
+    int                      nbTentatives;
+
+  public:
+    Joueur();
+    int  score() const;
+    int  incrementerScore();
+    int  tentativesRestantes() const;
+    char entreeJoueur();
+    void afficherScore() const;
+    void reinitialiserJeu();
+    void proposerMot(std::string motPropose);
 };
+
+#endif // JOUEUR_H
