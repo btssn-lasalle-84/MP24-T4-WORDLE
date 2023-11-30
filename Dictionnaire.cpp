520bcbfbd78d1d06b99974c0b012142ad4759326
TVIGNAL <thomasvignal.btssn@gmail.com>
2023-11-30 08:21:48 +0100

    MP24T4-4 Définition et déclaration de la classe Dictionnaire

--- Dictionnaire.cpp
+++ Dictionnaire.cpp
@@ -1,1 +1,16 @@
 #include "Dictionnaire.h"
+#include <iostream>
+
+using namespace std;
+
+Dictionnaire::Dictionnaire()
+{
+    listeMots = { "chat", "chien", "oiseau", "maison", "ordinateur" };
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+}
+
+std::string Dictionnaire::getMotAleatoire()
+{
+    int indiceAleatoire = std::rand() % listeMots.size();
+    return listeMots[indiceAleatoire];
+}
