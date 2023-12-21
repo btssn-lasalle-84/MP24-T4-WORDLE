6c8be177dc1725d63c5217dc60d571119ad830ff
TVIGNAL <thomasvignal.btssn@gmail.com>
2023-12-21 15:16:22 +0100

    MP24T4-39 Proposition de relancer partie et affichage en majuscule

--- Wordle.cpp
+++ Wordle.cpp
@@ -4,8 +4,7 @@
 
 #ifdef DEBUG_WORDLE
 #include <iostream>
-#include <algorithm> // Add this include for std::transform
-
+#include <algorithm>
 #endif
 
 Wordle::Wordle() :
@@ -22,56 +21,49 @@ Wordle::~Wordle()
 
 void Wordle::demarrerPartie()
 {
-    do
+    initialiserPartie();
+    ihmPartie->afficherNomWordle();
+    ihmPartie->afficherRegles();
+
+    int tentative = INCREMENTATION_TENTATIVES;
+    while(tentative <= NB_TENTATIVES_MAX)
     {
-        initialiserPartie();
-        ihmPartie->afficherNomWordle();
-        ihmPartie->afficherRegles();
-
-        int tentative = INCREMENTATION_TENTATIVES;
-        while(tentative <= NB_TENTATIVES_MAX)
+        std::string motSaisi = ihmPartie->saisirMot();
+        if(motSaisi.size() != TAILLE_MAX_MOT)
         {
-            std::string motSaisi = ihmPartie->saisirMot();
-            if(motSaisi.size() != TAILLE_MAX_MOT)
-            {
-                std::cerr << "Erreur : La taille du mot doit être exactement de " << TAILLE_MAX_MOT
-                          << " lettres." << std::endl;
-                continue;
-            }
-            if(motsDejaSaisis.find(motSaisi) != motsDejaSaisis.end())
-            {
-                std::cerr << "Erreur : Ce mot a déjà été saisi auparavant." << std::endl;
-                continue;
-            }
-
-            if(setMotEntre(motSaisi))
-            {
-                analyserMot();
-                motsDejaSaisis.insert(motSaisi);
-                joueur->proposerMot(motSaisi);
-                joueur->incrementerTentatives();
-                ihmPartie->afficherLettreEnCouleurSelonEtat();
-                if(estMotCorrect())
-                {
-#ifdef DEBUG_WORDLE
-                    std::cout << "Félicitations ! Vous avez deviné le mot." << std::endl;
-#endif
-                    break;
-                }
-            }
-            else
-            {
-            }
-            ++tentative;
+            std::cerr << "Erreur : La taille du mot doit être exactement de " << TAILLE_MAX_MOT
+                      << " lettres." << std::endl;
+            continue;
         }
-    } while(ihmPartie->demanderContinuerPartie());
-    ihmPartie->nbTentativesAtteint();
+        if(motsDejaSaisis.find(motSaisi) != motsDejaSaisis.end())
+        {
+            std::cerr << "Erreur : Ce mot a déjà été saisi auparavant." << std::endl;
+            continue;
+        }
+
+        if(setMotEntre(motSaisi))
+        {
+            analyserMot();
+            motsDejaSaisis.insert(motSaisi);
+            joueur->proposerMot(motSaisi);
+            joueur->incrementerTentatives();
+            ihmPartie->afficherLettreEnCouleurSelonEtat();
+            if(estMotCorrect())
+            {
+#ifdef DEBUG_WORDLE
+                std::cout << "Félicitations ! Vous avez deviné le mot." << std::endl;
+#endif
+                ihmPartie->demanderContinuerPartie();
+                break;
+            }
+        }
         else
         {
         }
         ++tentative;
-    } ihmPartie->nbTentativesAtteint();
-    } while (ihmPartie->demanderContinuerPartie());
+    }
+    ihmPartie->nbTentativesAtteint();
+    ihmPartie->demanderContinuerPartie();
 }
 
 void Wordle::analyserMot()
@@ -174,29 +166,27 @@ std::string Wordle::mettreLettreEnCouleurSelonEtat() const
 {
     std::string resultat;
 
-    for (size_t i = 0; i < motEntre.size(); ++i)
+    for(size_t i = 0; i < motEntre.size(); ++i)
     {
-        char lettre = motEntre[i];
-        EtatAnalyse etat = analyseMot[i];
+        char        lettre = std::toupper(motEntre[i]);
+        EtatAnalyse etat   = analyseMot[i];
 
-        switch (etat)
+        switch(etat)
         {
-        case EtatAnalyse::ABSENTE_ROUGE:
-            resultat += "\033[31m" + std::string(1, lettre) + "\033[0m"; // Rouge
-            break;
-        case EtatAnalyse::BIEN_PLACE_VERT:
-            resultat += "\033[32m" + std::string(1, lettre) + "\033[0m"; // Vert
-            break;
-        case EtatAnalyse::MAL_PLACE_JAUNE:
-            resultat += "\033[33m" + std::string(1, lettre) + "\033[0m"; // Jaune
-            break;
-        default:
-            resultat += std::string(1, lettre);
-            break;
+            case EtatAnalyse::ABSENTE_ROUGE:
+                resultat += "\033[31m" + std::string(1, lettre) + "\033[0m"; // Rouge
+                break;
+            case EtatAnalyse::BIEN_PLACE_VERT:
+                resultat += "\033[32m" + std::string(1, lettre) + "\033[0m"; // Vert
+                break;
+            case EtatAnalyse::MAL_PLACE_JAUNE:
+                resultat += "\033[33m" + std::string(1, lettre) + "\033[0m"; // Jaune
+                break;
+            default:
+                resultat += std::string(1, lettre);
+                break;
         }
     }
 
     return resultat;
 }
-
-
